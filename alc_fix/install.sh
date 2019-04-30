8d5520aa35ea8ab2ff05f1511330d3b5af19bf7c
Menchen <huangmenchen@gmail.com>
2019-04-30 18:57:43 +0200

    Fix typo

--- alc_fix/install.sh
+++ alc_fix/install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-sudo cp ALCPlugFix /usr/local/bin
+sudo cp ALCPlugFix /usr/local/bin/
 sudo chmod 755 /usr/local/bin/ALCPlugFix
 sudo chown $USER:admin /usr/local/bin/ALCPlugFix
-sudo cp hda-verb /usr/local/bin
+sudo cp hda-verb /usr/local/bin/
 sudo chmod 755 /usr/local/bin/hda-verb
 sudo chown $USER:admin /usr/local/bin/hda-verb
-sudo cp good.win.ALCPlugFix.plist /Library/LaunchAgents/
+sudo cp good.win.ALCPlugFix.plist /Library/LaunchDaemons/
 sudo chmod 644 /Library/LaunchDaemons/good.win.ALCPlugFix.plist
 sudo chown root:wheel /Library/LaunchDaemons/good.win.ALCPlugFix.plist
 sudo launchctl load /Library/LaunchDaemons/good.win.ALCPlugFix.plist
 
+echo "Done!"
 exit 0

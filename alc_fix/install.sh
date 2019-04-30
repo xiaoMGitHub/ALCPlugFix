aae3107f6f966842baf6797e5ddb37fd258c5750
Menchen <huangmenchen@gmail.com>
2019-04-30 14:36:58 +0200

    Fix typo in install file, also move to LaunchDaemons to launch before login

--- alc_fix/install.sh
+++ alc_fix/install.sh
@@ -7,8 +7,8 @@ sudo cp hda-verb /usr/local/bin
 sudo chmod 755 /usr/local/bin/hda-verb
 sudo chown $USER:admin /usr/local/bin/hda-verb
 sudo cp good.win.ALCPlugFix.plist /Library/LaunchAgents/
-sudo chmod 644 /Library/LaunchAgents/good.win.ALCPlugFix.plist
-sudo chown root:wheel /Library/LaunchAgents/good.win.ALCPlugFix.plist
-sudo launchctl load /Library/LaunchAgents/good.win.ALCPlugFix.plist
+sudo chmod 644 /Library/LaunchDaemons/good.win.ALCPlugFix.plist
+sudo chown root:wheel /Library/LaunchDaemons/good.win.ALCPlugFix.plist
+sudo launchctl load /Library/LaunchDaemons/good.win.ALCPlugFix.plist
 
 exit 0

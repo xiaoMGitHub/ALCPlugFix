2a60cebf36b520bf9e5ba8feea4c0cd8360308c2
Sniki <30008333+Snikii@users.noreply.github.com>
2019-08-10 23:39:56 +0200

    Add ALC3232 data

--- ALCPlugFix/main.m
+++ ALCPlugFix/main.m
@@ -132,8 +132,8 @@ void sigHandler(int signo)
 
 void fixAudio(){
     NSLog(@"Fixing...");
-    NSString *output1 = [[binPrefix stringByAppendingString:@"hda-verb 0x18 SET_PIN_WIDGET_CONTROL 0x22"] runAsCommand];
-    NSString *output2 = [[binPrefix stringByAppendingString:@"hda-verb 0x21 SET_UNSOLICITED_ENABLE 0x83"] runAsCommand];
+    NSString *output1 = [[binPrefix stringByAppendingString:@"hda-verb 0x1a SET_PIN_WIDGET_CONTROL 0x24"] runAsCommand];
+    NSString *output2 = [[binPrefix stringByAppendingString:@"hda-verb 0x15 SET_UNSOLICITED_ENABLE 0x83"] runAsCommand];
 }
 
 

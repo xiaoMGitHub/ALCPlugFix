b6413915028f97287290c8edeb88dfc9da57c38f
Joshua Seltzer <joshua.seltzer90@gmail.com>
2019-09-12 16:47:16 -0400

    Revamping the install script to allow upgrades.  Added an uninstall script for anyone wanting to completely remove it. Added gitignore file.

--- alc_fix/install.sh
+++ alc_fix/install.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
 
-sudo cp ALCPlugFix /usr/bin/
-sudo chmod 755 /usr/bin/ALCPlugFix
-sudo chown $USER:admin /usr/bin/ALCPlugFix
-sudo cp hda-verb /usr/bin/
-sudo chmod 755 /usr/bin/hda-verb
-sudo chown $USER:admin /usr/bin/hda-verb
-sudo cp good.win.ALCPlugFix.plist /Library/LaunchDaemons/
-sudo chmod 644 /Library/LaunchDaemons/good.win.ALCPlugFix.plist
-sudo chown root:wheel /Library/LaunchDaemons/good.win.ALCPlugFix.plist
-sudo launchctl load /Library/LaunchDaemons/good.win.ALCPlugFix.plist
+echo "Installing ALCPlugFix..."
+
+DAEMON_PATH=/Library/LaunchDaemons/
+BIN_PATH=/usr/bin/
+DAEMON_FILE=good.win.ALCPlugFix.plist
+VERB_FILE=hda-verb
+FIX_FILE=ALCPlugFix
+
+# stop the daemon if it's already running
+if sudo launchctl list | grep --quiet ALCPlugFix; then
+    echo "Stopping existing ALCPlugFix daemon."
+    sudo launchctl unload $DAEMON_PATH$DAEMON_FILE
+fi
+
+# copy over the files to their respective locations (overwrite automatically if files exist)
+sudo cp -f ALCPlugFix $BIN_PATH
+sudo cp -f hda-verb $BIN_PATH
+sudo cp -f good.win.ALCPlugFix.plist $DAEMON_PATH
+
+# set permissions and ownership
+sudo chmod 755 $BIN_PATH$FIX_FILE
+sudo chown $USER:admin $BIN_PATH$FIX_FILE
+sudo chmod 755 $BIN_PATH$VERB_FILE
+sudo chown $USER:admin $BIN_PATH$VERB_FILE
+sudo chmod 644 $DAEMON_PATH$DAEMON_FILE
+sudo chown root:wheel $DAEMON_PATH$DAEMON_FILE
+
+# load and start the daemon
+sudo launchctl load -w $DAEMON_PATH$DAEMON_FILE
 
 echo "Done!"
 exit 0
